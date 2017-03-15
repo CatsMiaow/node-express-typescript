c76a8c62722dea7ef84d582dcc291500f7dd0ade
Kang <infiltrator@naver.com>
2017-03-15 13:56:59 +0900

    Add global type

---
+++ typings/global.d.ts
@@ -0,0 +1,13 @@
+// https://www.typescriptlang.org/docs/handbook/declaration-files/templates/global-modifying-module-d-ts.html
+
+declare global {
+    namespace Animal {
+      interface Cat {
+        feature: string;
+      }
+    }
+
+    interface Human {}
+}
+
+export { };
